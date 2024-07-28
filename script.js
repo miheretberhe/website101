d833da7519522a45912418e19f3654dbfb7fbb17
Miheret Berhe <158329965+miheretberhe@users.noreply.github.com>
2024-07-28 09:35:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick =() =>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+Let navbar = document.querySelector('.navbar');
+
+let navbar = document.querySelector('.#menu-btn').onclick= ()=>{
+    loginForm.classList.toggle('active');
+    navbar.classForm.remove('active');
+}
+
+window.onscroll =()=>{
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+    
